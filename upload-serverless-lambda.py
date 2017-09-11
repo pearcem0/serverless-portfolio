1137a5073bf2cf929a11bbaede8d11b2c64098f2
Michael Pearce <michael@rootofpi.co.uk>
2017-09-11 23:28:05 +0100

    Updated serverless deployment lambda script

--- upload-serverless-lambda.py
+++ upload-serverless-lambda.py
@@ -3,18 +3,21 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('serverless.rootofpi.co.uk')
-build_bucket = s3.Bucket('serverless-portfolio')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    portfolio_bucket = s3.Bucket('serverless.rootofpi.co.uk')
+    build_bucket = s3.Bucket('serverless-portfolio')
 
-portfolio_zip = StringIO.StringIO()
+    portfolio_zip = StringIO.StringIO()
 
-build_bucket.download_fileobj('serverlessbuild.zip',portfolio_zip)
+    build_bucket.download_fileobj('serverlessbuild.zip',portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-          # Remember mimetypes guesses using file extension so make sure exists!
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+            # Remember mimetypes guesses using file extension so make sure exists!
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Job Complete."
