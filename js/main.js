1e6e04cecd6c76238e8efddf8226393fddc0e8d5
Michael Pearce <michael@rootofpi.co.uk>
2017-09-20 18:30:22 +0100

    Configured webpack and  package.json

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import 'react-dom'
+
+// for good measure (and practice) at this point
+console.log("loaded React-dom");
+
+console.log("webpack is watching");
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  // where webpack will start looking for the code
+  entry: './js/main.js',
+  // where webpack will output when it's done
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  // tell webpack HOW to package js, depedencies etc.
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      // which loader(plugin) to use to know how to build the code
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react']
+        }
+      }
+    }]
+  }
+}
