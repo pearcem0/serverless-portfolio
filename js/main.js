f5ca90d88d72a831373cb7d10542844bbe8802aa
Michael Pearce <michael@rootofpi.co.uk>
2017-09-21 13:15:53 +0100

    mend

--- js/main.js
+++ js/main.js
@@ -5,4 +5,33 @@ import ExampleWork from './example-work';
 
 console.log("logging");
 
-ReactDOM.render(<ExampleWork />, document.getElementById('example-work'));
+const myWork = [
+  {
+    'title': "All You Need",
+    'image': {
+      'desc': "example image of a cat.",
+      'source': "images/example3.png",
+      'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+           https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  },
+  {
+    'title': "AWS Lambda Snapshot Backup & Prune",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'source': "images/example1.png",
+      'comment': "probably doesn't even compile"
+    }
+  },
+  {
+    'title': "Salesforce Administration Areas",
+    'image': {
+      'desc': "chemistry shots",
+      'source': "images/example2.png",
+      // use backticks to use javascript template string, on multi line
+      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+          https://www.flickr.com/photos/ssoosay/4097410999 `
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
